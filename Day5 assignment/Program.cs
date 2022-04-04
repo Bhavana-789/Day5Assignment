3936af487210f644bed8b11b509b0dd70095f12a
Bhavana <bhavananerlakota@gmail.com>
2022-04-04 16:14:16 +0530

    Added UC5 Factors of a number program

--- Day5 assignment/Program.cs
+++ Day5 assignment/Program.cs
@@ -7,28 +7,27 @@ namespace Day5_assignment
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic C# programs");
-            //UC4_NthHarmonic
-            Console.WriteLine("Nth Harmonic Program");
-            int num;
-            Console.Write("Enter the value of N: ");
-            num = Convert.ToInt32(Console.ReadLine());
-            double result = 0;
-            for (double i = 1; i <= num; i++)
+            //UC5_Factors
+            Console.WriteLine("Program to find Factors of a number");
+            int a, b;
+            Console.Write("Enter the Number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            for (b = 1; b <= a; b++)
             {
-                result += 1 / i;
+                if (a % b == 0)
+                {
+                    Console.WriteLine(b + "is a factor of" + a);
+
+                }
             }
-            Console.WriteLine(Math.Round(result, 2));
-            if (num == 0)
-            {
-                Console.WriteLine("Invalid Input");
-            }
-            else
-            {
-                Console.WriteLine("The Nth Harmonic Number is: " + result);
-            }
+            Console.ReadLine();
         }
     }
 }
+
+    
+    
+
 
         
     
