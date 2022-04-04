6e05f3366f3ee696b64be87db1ac04248cb4d52a
Bhavana <bhavananerlakota@gmail.com>
2022-04-04 16:29:13 +0530

    Added UC8 Even Or Odd Program

--- Day5 assignment/Program.cs
+++ Day5 assignment/Program.cs
@@ -7,26 +7,26 @@ namespace Day5_assignment
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to basic C# programs");
-            //UC7_Swapping
-            Console.WriteLine("Pragram to swap a number");
-            int num1, num2;
-            Console.Write("Enter First Number: ");
-            num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter Second Number: ");
-            num2 = Convert.ToInt32(Console.ReadLine());
-
-            int temp;
-            temp = num1;
-            num1 = num2;
-            num2 = temp;
-
-            Console.WriteLine("After Swapping:");
-            Console.Write("First Number :{0} \nSecond Number :{1}", num1, num2);
-
-            Console.ReadKey();
+            //UC8_EvenOrOdd
+            Console.WriteLine("Pragram to find Even or Odd of given number");
+            int x;
+            Console.WriteLine("Enter a Number :");
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Entered Number is an Even Number");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Entered Number is an Odd Number");
+                Console.ReadLine();
+            }
         }
     }
 }
+    
+
 
         
     
