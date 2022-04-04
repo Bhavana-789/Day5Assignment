3bf0404d2b4d23e1f4aa49a37631a3eee9d7a29d
Bhavana <bhavananerlakota@gmail.com>
2022-04-04 15:19:59 +0530

    added day5 assignment

---
+++ Day5 assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day5_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic C# programs");
+        }
+    }
+}
+            
+
+
+
+           
+
+
+        
+    
+
