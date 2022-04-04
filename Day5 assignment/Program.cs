ead86dc42c518f4d0280e516385e9d4f58f3c685
Bhavana <bhavananerlakota@gmail.com>
2022-04-04 15:48:44 +0530

    Added UC2 LeapYear Program

--- Day5 assignment/Program.cs
+++ Day5 assignment/Program.cs
@@ -7,52 +7,34 @@ namespace Day5_assignment
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic C# programs");
-            //UC1
-            Console.WriteLine("Welcome to Flip Coin Program");
-
-            int count = 0;
-            int Heads = 0;
-            int Tails = 0;
-            while (count < 100)
+            //UC2_LeapYear
+            Console.WriteLine("Program to Check LeapYear");
+            Console.WriteLine("Enter year:");
+            int year = int.Parse(Console.ReadLine());
+            if (((year % 4 == 0) && (year % 100! == 0)) || (year % 400 == 0))
             {
-                Random random = new Random();
-                int result = random.Next(0, 2);
-                if (result == 0)
-                {
-                    Console.WriteLine("Heads!");
-                    Heads = Heads + 1;
-                    count = count + 1;
-
-                }
-                else
-                {
-                    Console.WriteLine("Tails!");
-                    Tails = Tails + 1;
-                    count = count + 1;
-                }
+                Console.WriteLine("{0} is a Leap Year", year);
             }
-            Console.WriteLine("Heads=" + Heads + "Tails=" + Tails + "count=" + count);
-            int totaltails = count - Heads;
-            int totalheads = count - Tails;
-            Console.WriteLine("total heads is" + totalheads + "total tails is" + totaltails);
-            float headPercent = ((float)totalheads / count) * 100;
-            float tailPercent = ((float)totaltails / count) * 100;
-            Console.WriteLine("The percentage of heads is:" + Math.Round(headPercent, 2)
-                             + "The percentage of tails is:" + Math.Round(tailPercent, 2));
+            else
+            {
+                Console.WriteLine("{0} is not a Leap year", year);
+            }
+
         }
-
-
-
     }
 }
 
-            
 
 
 
-           
 
 
-        
-    
+
+
+
+
+
+
+
+
 
